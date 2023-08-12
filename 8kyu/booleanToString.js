1bc17ec22199953a4ab5b00638b57b8521472e2a
prinsi-t <tprincy56@gmail.com>
2023-08-12 16:38:25 +0530

    did more codewars

---
+++ 8kyu/booleanToString.js
@@ -0,0 +1,12 @@
+function booleanToString(b){
+    //your code here
+
+    /* if (b === true){
+        return 'true';
+    }else(b === false)
+        return 'false'; */
+    return b ? 'true' : 'false';
+    
+  }
+
+  console.log(booleanToString(false));
