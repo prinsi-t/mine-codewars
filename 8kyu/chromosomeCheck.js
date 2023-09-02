f8ecbc0f19bf2fc341b48116d36af4b652998f20
prinsi-t <tprincy56@gmail.com>
2023-09-02 19:29:16 +0530

    codewars

---
+++ 8kyu/chromosomeCheck.js
@@ -0,0 +1,8 @@
+function chromosomeCheck(sperm) {
+  if(sperm === 'XX'){
+    return "Congratulations! You're going to have a daughter.";
+  }
+    return "Congratulations! You're going to have a son.";
+  
+}
+chromosomeCheck('XX')
