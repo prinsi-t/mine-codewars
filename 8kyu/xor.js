3c1ae7b6a295eb18440126ee1bd0b5eca1f1263b
prinsi-t <tprincy56@gmail.com>
2023-09-11 18:30:41 +0530

    codewars

---
+++ 8kyu/xor.js
@@ -0,0 +1,11 @@
+function xor(a, b) {
+    // TODO: Program Me
+    if (a === true){
+        return true;
+    }else if( b === true){
+        return true;
+    }else{
+        return false;
+    }
+  }
+  console.log(xor(true, true));
