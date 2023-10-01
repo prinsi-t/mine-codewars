282f314876e766befd2f240dadc22c62a7fe93e2
prinsi-t <tprincy56@gmail.com>
2023-10-01 19:14:08 +0530

    codewars

---
+++ 8kyu/isUpperCase.js
@@ -0,0 +1,11 @@
+String.prototype.isUpperCase = function() {
+    // your code here
+    if(str === str.toUpperCase()){
+        return true;
+    }
+        return false
+    
+
+  }
+  let str = 'hello , I amd F';  
+  str.isUpperCase()
