246a3f6780638e5a3eeb62c5dd90c4a0956ef09b
prinsi-t <tprincy56@gmail.com>
2023-11-14 17:46:34 +0530

    codewars

---
+++ 8kyu/century.js
@@ -0,0 +1,10 @@
+function century(year) {
+    // Finish this :)
+    if(year <= 100){
+        return 1;
+    }else if(year % 100 === 0){
+        return parseInt(year/ 100);
+    }else 
+    return parseInt(year / 100) + 1;
+  }
+  console.log(century(1705));
