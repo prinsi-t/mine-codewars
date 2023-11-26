f7ee3a1051fa883f707a7719a3b720cc47868323
prinsi-t <tprincy56@gmail.com>
2023-11-26 20:47:20 +0530

    codewars

---
+++ 8kyu/oddCount.js
@@ -0,0 +1,10 @@
+function oddCount(n){
+    // your code here
+    let odd = '';
+    for (const number of odd){
+        if (n % 2 != 0){
+            return number;
+        }
+    }
+  }
+  console.log(oddCount(15));
