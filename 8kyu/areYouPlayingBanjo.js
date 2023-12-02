48eca1fc28f5cf9e962262bd438bddb1869629f7
prinsi-t <tprincy56@gmail.com>
2023-12-02 21:45:00 +0530

    codewars

---
+++ 8kyu/areYouPlayingBanjo.js
@@ -0,0 +1,11 @@
+function areYouPlayingBanjo(name) {
+    // Implement me
+    if(name[0] === 'R' || name[0] === 'r'){
+        return `${name} plays banjo`;
+    }else{
+        return `${name} does not play banjo`;
+    }
+    
+  }
+
+  
