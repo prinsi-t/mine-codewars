0f52995c33cfebfffc7e89634586f89a650d5218
prinsi-t <tprincy56@gmail.com>
2023-12-15 17:17:34 +0530

    codewars

---
+++ 8kyu/monkeyCount.js
@@ -0,0 +1,11 @@
+function monkeyCount(n) {
+    // your code here
+    let count = [];
+
+    for (let i = 1; i <= n; i++){
+        count.push(i);
+
+    }
+    return count;
+    }
+    console.log(monkeyCount(5));
