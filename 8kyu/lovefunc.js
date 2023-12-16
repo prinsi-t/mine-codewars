719f204b43f77806e76272df50b1aa0c783686ce
prinsi-t <tprincy56@gmail.com>
2023-12-16 17:15:09 +0530

    codewars

--- 8kyu/lovefunc.js
+++ 8kyu/lovefunc.js
@@ -1,5 +1,5 @@
 function lovefunc(flower1, flower2){
-    //code.....
+    //code here....
     if(flower1 % 2 === 0 && flower2 % 2 != 0){
         return true;
     }else if(flower2 % 2 === 0 && flower1 % 2 != 0){
