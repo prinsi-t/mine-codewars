cc4483e08ea49fdeb9ceb97350b3b1af7174af4e
prinsi-t <tprincy56@gmail.com>
2023-12-24 19:30:02 +0530

    codewars

---
+++ 8kyu/goals.js
@@ -0,0 +1,5 @@
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    // code goes here
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+  }
+  console.log(goals(0, 0, 0));
