ceff741d9a543f0b36387fc3c01610539bd9e558
prinsi-t <tprincy56@gmail.com>
2023-12-27 17:32:17 +0530

    codewars

---
+++ 8kyu/defineSuit.js
@@ -0,0 +1,11 @@
+function defineSuit(card) {
+
+    let suit = card.substr(-1);
+    switch(suit){
+       case '♣': return 'clubs';
+       case '♦': return 'diamonds';
+       case '♥': return 'hearts';
+       default: return 'spades';
+    }
+ 
+ }
