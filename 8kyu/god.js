5b0cfb467a882f9336fad7a08213760ee5ac05c9
prinsi-t <tprincy56@gmail.com>
2024-02-20 20:09:06 +0530

    codewars

---
+++ 8kyu/god.js
@@ -0,0 +1,21 @@
+class Human {}
+class Man extends Human {}
+class Woman extends Human {}
+
+
+class God{
+/**
+ * @returns Human[]
+
+ */
+
+  static create(){
+    // code
+    return [new Man(), new Woman()];
+
+  }
+
+}
+
+// code
+ 
